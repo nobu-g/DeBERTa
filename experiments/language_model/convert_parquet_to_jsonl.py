b8695be33fd0e4f4740baa1bf3ea7669755f3ac4
nobu-g <ueda-n127@hotmail.co.jp>
2024-01-25 22:49:05 +0900

    fix convert_parquet_to_jsonl.py

--- experiments/language_model/convert_parquet_to_jsonl.py
+++ experiments/language_model/convert_parquet_to_jsonl.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
-from datasets import Dataset, disable_caching
+from datasets import Dataset
 from tqdm import tqdm
 from utils import list_input_files
 
@@ -11,14 +11,22 @@ logger = logging.getLogger(__name__)
 disable_caching()
 
 
-def process_file(input_file: Path, output_dir: Path, overwrite: bool) -> None:
-    logger.info(f"Writing the dataset to {output_dir} in JSONL format.")
-    output_file: Path = output_dir.joinpath(f"{input_file.stem}.jsonl")
+def process_file(input_file: Path, input_format: str, output_dir: Path, output_format: str, overwrite: bool) -> None:
+    logger.info(f"Writing the dataset to {output_dir} in {output_format} format.")
+    output_file: Path = output_dir.joinpath(f"{input_file.stem}.{output_format}")
     if output_file.exists() and not overwrite:
         logger.error(f"{output_file} already exists. Specify --overwrite to overwrite.")
         return
-    dataset: Dataset = Dataset.from_parquet(str(input_file), keep_in_memory=True)
-    dataset.to_json(output_file, force_ascii=False)
+    if input_format == "jsonl":
+        dataset = Dataset.from_json(str(input_file), keep_in_memory=True)
+    else:
+        assert input_format == "parquet"
+        dataset = Dataset.from_parquet(str(input_file), keep_in_memory=True)
+    if output_format == "jsonl":
+        dataset.to_json(output_file, force_ascii=False)
+    else:
+        assert output_format == "parquet"
+        dataset.to_parquet(output_file)
     logger.info(f"Finished exporting to {output_file}.")
 
 
@@ -32,10 +40,24 @@ def main() -> None:
         help="Path(s) to the input data directory or file.",
     )
     parser.add_argument(
+        "--input-format",
+        type=str,
+        default="parquet",
+        choices=["jsonl", "parquet"],
+        help="Input format.",
+    )
+    parser.add_argument(
         "--output-dir",
         "-o",
         type=str,
         help="Path to the output directory.",
+    )
+    parser.add_argument(
+        "--output-format",
+        type=str,
+        default="jsonl",
+        choices=["jsonl", "parquet"],
+        help="Output format.",
     )
     parser.add_argument(
         "--overwrite",
@@ -54,8 +76,8 @@ def main() -> None:
     output_dir.mkdir(parents=True, exist_ok=True)
 
     with ProcessPoolExecutor(max_workers=args.num_proc) as executor:
-        for input_file in tqdm(list_input_files(args.input_path)):
-            executor.submit(process_file, input_file, output_dir, args.overwrite)
+        for input_file in tqdm(list_input_files(args.input_path, args.input_format)):
+            executor.submit(process_file, input_file, args.input_format, output_dir, args.output_format, args.overwrite)
 
 
 if __name__ == "__main__":
